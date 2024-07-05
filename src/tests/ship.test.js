bd436f5dfe36845d6bedb99f55ac0fc40efee15c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-05 12:33:27 +0200

    test: add other tests for ship.js

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,9 +1,19 @@
 import { Ship } from '../ship';
 
 describe('Ship', () => {
-  const ship = new Ship();
+  let ship;
+  beforeEach(() => {
+    ship = new Ship(2);
+  });
   test('increase hits correctly', () => {
     ship.hit();
     expect(ship.hits).toBe(1);
+  });
+
+  test('isSunk returns true when ship Sunk', () => {
+    ship.hit();
+    expect(ship.isSunk()).toBeFalsy();
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy();
   });
 });
