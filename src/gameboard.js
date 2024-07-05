7d8166bef18bfd6e87fbed62a346124e534abfe9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-05 13:09:01 +0200

    feat: add board and functions to create/return it

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,1 +1,20 @@
-export function Gameboard() {}
+export function Gameboard() {
+  const createBoard = () => {
+    const size = 10;
+    const board = [];
+    for (let i = 0; i < size; i++) {
+      const row = [];
+      for (let j = 0; j < size; j++) {
+        row[j] = null;
+      }
+      board[i] = row;
+    }
+    return board;
+  };
+
+  const board = createBoard();
+
+  const getBoard = () => board;
+
+  return { getBoard };
+}
