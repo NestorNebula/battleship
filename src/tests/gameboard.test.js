d78446cae8c8a38f26df39248f929982917e90a3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-05 15:03:12 +0200

    test: add tests for the future placeShips function

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -9,4 +9,14 @@ describe('Gameboard', () => {
     expect(board[4][6]).toBeNull();
     expect(board[7][9]).toBeNull();
   });
+
+  test('place ships on correct coordinates', () => {
+    const board = gameboard.getBoard();
+    gameboard.placeShips();
+    expect(board[0][4]).toBe(0);
+    expect(board[1][3]).toBe(1);
+    expect(board[2][2]).toBe(2);
+    expect(board[3][1]).toBe(3);
+    expect(board[4][0]).toBe(4);
+  });
 });
