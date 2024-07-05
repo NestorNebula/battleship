5f90c8e90fac04d8fc391ca18f0e02c956c57d95
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-05 13:00:46 +0200

    test: add tests for Gameboard

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,1 +1,12 @@
 import { Gameboard } from '../gameboard';
+
+describe('Gameboard', () => {
+  const gameboard = Gameboard();
+
+  test('gameboard created correctly', () => {
+    const board = gameboard.getBoard();
+    expect(board[0][1]).toBeNull();
+    expect(board[4][6]).toBeNull();
+    expect(board[7][9]).toBeNull();
+  });
+});
