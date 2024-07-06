dd58f0edce5757a0ade970fe0e9f9ef47166abed
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-06 16:44:49 +0200

    test: add tests for upcoming Computer subclass

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,8 +1,23 @@
-import { Player } from '../player';
+import { Player, Computer } from '../player';
 
 describe('Player', () => {
   const player = new Player();
   test('player has a board', () => {
     expect(player.board.getBoard()).toBeDefined();
+  });
+});
+
+describe('Computer', () => {
+  const computer = new Computer();
+  test('computer has a board', () => {
+    expect(computer.board.getBoard()).toBeDefined();
+  });
+
+  test('random coordinates returns correct coordinates', () => {
+    const coordinates = computer.randomCoordinates();
+    expect(coordinates[0]).toBeGreaterThanOrEqual(0);
+    expect(coordinates[0]).toBeLessThan(10);
+    expect(coordinates[1]).toBeGreaterThanOrEqual(0);
+    expect(coordinates[1]).toBeLessThan(10);
   });
 });
