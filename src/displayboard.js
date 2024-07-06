130e0a035e4f14c2e6bdfe0b3c160b113e97e785
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-06 11:29:08 +0200

    feat: add (display) playerBoard function

---
+++ src/displayboard.js
@@ -0,0 +1,23 @@
+export function Displayboard() {
+  const playerBoard = (board) => {
+    const domBoard = document.querySelector('.board');
+    let rows = 0;
+    board.forEach((row) => {
+      let squares = 0;
+      const domRow = document.createElement('div');
+      domRow.classList.add('row');
+      row.forEach((square) => {
+        const domSquare = document.createElement('button');
+        domSquare.classList.add('square');
+        domSquare.setAttribute('id', `${rows}-${squares}`);
+        domSquare.textContent = square;
+        domRow.appendChild(domSquare);
+        squares += 1;
+      });
+      domBoard.appendChild(domRow);
+      rows += 1;
+    });
+  };
+
+  return { playerBoard };
+}
--- src/game.js
+++ src/game.js
@@ -1,6 +1,11 @@
 import { Player } from './player';
+import { Displayboard } from './displayboard';
 
 export function Game() {
   const player = new Player();
   const computer = new Player();
+  const display = Displayboard();
+
+  player.board.placeShips();
+  display.playerBoard(player.board.getBoard());
 }
