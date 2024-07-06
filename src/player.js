9fe55f71465f71f99c75d361565bd01c99c71d53
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-06 16:50:31 +0200

    feat: create new Computer subclass extends Player

--- src/player.js
+++ src/player.js
@@ -1,7 +1,21 @@
 import { Gameboard } from './gameboard';
 
-export class Player {
+class Player {
   constructor() {
     this.board = Gameboard();
   }
 }
+
+class Computer extends Player {
+  constructor() {
+    super();
+  }
+
+  randomCoordinates() {
+    const a = Math.floor(Math.random() * 9);
+    const b = Math.floor(Math.random() * 9);
+    return [a, b];
+  }
+}
+
+export { Player, Computer };
