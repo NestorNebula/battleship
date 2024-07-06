161edf20627b719b6015014b0c8486223a11326a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-06 16:01:49 +0200

    feat: add startGame and waitMove functions

--- src/game.js
+++ src/game.js
@@ -7,7 +7,26 @@ export function Game() {
   const display = Displayboard();
 
   player.board.placeShips();
+  computer.board.placeShips();
   const playerFleet = player.board.getFleet();
   display.playerBoard(player.board.getBoard(), playerFleet);
   display.opponentBoard(computer.board.getBoard());
+
+  const startGame = () => {
+    waitMove();
+  };
+
+  const waitMove = () => {
+    const oppsquares = document.querySelectorAll('.oppsquare');
+    oppsquares.forEach((square) => {
+      square.addEventListener('click', () => {
+        const coordinates = square.id.split('-');
+        const result = computer.board.receiveAttack(coordinates);
+        display.opponentBoard(computer.board.getBoard());
+        waitMove();
+      });
+    });
+  };
+
+  startGame();
 }
