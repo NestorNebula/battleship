28b00669b784b00289cfae1e283d07c3897a77a9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-07 15:24:58 +0200

    feat: add placeShip function

--- src/gameboard.js
+++ src/gameboard.js
@@ -25,34 +25,65 @@ export function Gameboard() {
       coordinates: [0, 0],
       direction: 'horizontal',
       id: 0,
+      isPlaced: false,
     },
     {
       ship: new Ship(4),
       coordinates: [1, 0],
       direction: 'horizontal',
       id: 1,
+      isPlaced: false,
     },
     {
       ship: new Ship(3),
       coordinates: [2, 0],
       direction: 'horizontal',
       id: 2,
+      isPlaced: false,
     },
     {
       ship: new Ship(3),
       coordinates: [3, 0],
       direction: 'horizontal',
       id: 3,
+      isPlaced: false,
     },
     {
       ship: new Ship(2),
       coordinates: [4, 0],
       direction: 'horizontal',
       id: 4,
+      isPlaced: false,
     },
   ];
 
   const getFleet = () => fleet;
+
+  const placeShip = (col, row, direction) => {
+    let shipId = 0;
+    while (fleet[shipId].isPlaced === true) {
+      if (shipId >= fleet.length) break;
+      shipId += 1;
+    }
+    const shipCoordinates = [];
+    for (let i = 0; i < fleet[shipId].ship.length; i++) {
+      if (direction === 'horizontal') {
+        shipCoordinates.push([row, col + i]);
+      } else {
+        shipCoordinates.push([row + i, col]);
+      }
+    }
+    if (squaresEmpty(shipCoordinates)) {
+      shipCoordinates.forEach((square) => {
+        board[square[0]][square[1]].ship = shipId;
+      });
+      fleet[shipId].coordinates = [row, col];
+      fleet[shipId].isPlaced = true;
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   const placeShips = () => {
     const board = getBoard();
@@ -103,5 +134,12 @@ export function Gameboard() {
     return fleet.every((ship) => ship.ship.isSunk() === true);
   };
 
-  return { getBoard, getFleet, placeShips, receiveAttack, fleetSunk };
+  return {
+    getBoard,
+    getFleet,
+    placeShip,
+    placeShips,
+    receiveAttack,
+    fleetSunk,
+  };
 }
