4e04bb92d164c9a52644aba58d8fc04ebdb49931
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-09 12:03:49 +0200

    feat: add chooseSquare function

--- src/player.js
+++ src/player.js
@@ -9,12 +9,32 @@ class Player {
 class Computer extends Player {
   constructor() {
     super();
+    this.lastHit = null;
   }
 
   randomCoordinates() {
     const a = Math.floor(Math.random() * 10);
     const b = Math.floor(Math.random() * 10);
     return [a, b];
+  }
+
+  chooseSquare() {
+    if (this.lastHit === null) {
+      return this.randomCoordinates();
+    }
+    const randomDirection = Math.floor(Math.random() * 4);
+    switch (randomDirection) {
+      case 0:
+        return [this.lastHit[0] - 1, this.lastHit[1]];
+      case 1:
+        return [this.lastHit[0] + 1, this.lastHit[1]];
+      case 2:
+        return [this.lastHit[0], this.lastHit[1] - 1];
+      case 3:
+        return [this.lastHit[0], this.lastHit[1] + 1];
+      default:
+        throw Error('The computer cannot choose a square.');
+    }
   }
 }
 
